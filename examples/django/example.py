512935f2868fd46949c73cbb59cf8ece237d343b
Timothy McFadden <tim@timandjamie.com>
2019-08-26 11:22:01 -0600

    refactoring django example

--- examples/django/example.py
+++ examples/django/example.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
+# Django setup #######################################################
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 import django  # noqa: E402
 
 django.setup()
+#######################################################################
 
 
-from polls.models import Question, Choice  # noqa: E402
-from ppsql import django_ppsql as ppsql  # noqa: E402
+if __name__ == "__main__":
+    from polls.models import Question, Choice  # noqa: E402
+    from ppsql import ppsql  # noqa: E402
 
-ppsql(Question.objects.all())
-ppsql(Choice.objects.filter(votes__gte=10))
+    ppsql(Question.objects.all())
+    ppsql(Choice.objects.filter(votes__gte=10))
